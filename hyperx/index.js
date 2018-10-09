0bbc75478667c418495c2bbed1d10b8be2662fe6
Simone <simone.sacchi@nubilaria.com>
2018-10-10 00:10:10 +0200

    add hyperscript

--- hyperx/index.js
+++ hyperx/index.js
@@ -1,3 +1,4 @@
+// =========== BROWSER
 var vdom = require('virtual-dom')
 var hyperx = require('hyperx')
 var hx = hyperx(vdom.h)
@@ -13,3 +14,20 @@ var tree = hx`<div>
 })}
 </div>`
 console.log(vdom.create(tree).toString())
+
+// =========== NODE
+var h = require('hyperscript')
+var hyperx = require('hyperx')
+var hx = hyperx(h)
+
+var title = 'world'
+var wow = [1,2,3]
+var tree = hx`<div>
+  <h1 data-y="ab${1+2}cd">hello ${title}!</h1>
+  ${hx`<i>cool</i>`}
+  wow
+  ${wow.map(function (w) {
+    return hx`<b>${w}</b>\n`
+})}
+</div>`
+console.log(tree.outerHTML)
